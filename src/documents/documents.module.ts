a04322c10264e32fa048a78c33da61e37817befb
Andrey Zelenyy <zelenyandrei@gmail.com>
2022-08-07 18:50:46 +0300

    Регистрация сущности Document в модуле document с помощью метода TypeOrmModule.forFeature()

--- src/documents/documents.module.ts
+++ src/documents/documents.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Document } from './entities/document.entity';
 
-@Module({})
+@Module({
+    imports: [TypeOrmModule.forFeature([Document])],
+})
 export class DocumentsModule {}
