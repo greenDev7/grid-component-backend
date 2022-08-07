0f859ecb7d4e2f01db8796c5f57ff95fdf082ff0
Andrey Zelenyy <zelenyandrei@gmail.com>
2022-08-07 19:13:45 +0300

    Настройка подключения к sqlite в корневом модуле проекта

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { DocumentsModule } from './documents/documents.module';
 
 @Module({
-  imports: [DocumentsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: './test_db.sqlite',
+      synchronize: true,
+      autoLoadEntities: true,
+    }), 
+    DocumentsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
