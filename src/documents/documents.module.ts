5b8b4cb6ff4a0805f6c0be7690918cd8c878f503
Andrey Zelenyy <zelenyandrei@gmail.com>
2022-08-14 16:07:52 +0300

    Результат создания контроллера documents

--- src/documents/documents.module.ts
+++ src/documents/documents.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Document } from './entities/document.entity';
+import { DocumentsController } from './documents.controller';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Document])],
+    controllers: [DocumentsController],
 })
 export class DocumentsModule {}
