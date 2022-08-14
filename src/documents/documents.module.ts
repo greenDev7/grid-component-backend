c31bdbdde28c239dfd53cf5d3772954c4c8ac2e4
Andrey Zelenyy <zelenyandrei@gmail.com>
2022-08-14 16:54:59 +0300

    Результат создания сервиса documents

--- src/documents/documents.module.ts
+++ src/documents/documents.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Document } from './entities/document.entity';
 import { DocumentsController } from './documents.controller';
+import { DocumentsService } from './documents.service';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Document])],
     controllers: [DocumentsController],
+    providers: [DocumentsService],
 })
 export class DocumentsModule {}
