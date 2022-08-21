e8fa8f5ad6747b1a09b0da649609a6f75cb2f036
Andrey Zelenyy <zelenyandrei@gmail.com>
2022-08-21 12:42:06 +0300

    Реализация API по чтению всех документов из БД и их созданию

--- src/documents/documents.controller.ts
+++ src/documents/documents.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { DocumentsService } from './documents.service';
+import { CreateDocumentDto } from './dto/create-document.dto';
+import 
+{ Body, 
+  Controller, 
+  Get, 
+  HttpException, 
+  HttpStatus, 
+  Post 
+} from '@nestjs/common';
 
 @Controller('documents')
-export class DocumentsController {}
+export class DocumentsController {
+
+  constructor(private readonly documentService: DocumentsService) { }
+
+  @Get()
+  findAll() {
+    return this.documentService.findAllDocuments();
+  }
+
+  @Post()
+  create(@Body() createDocumentDto: CreateDocumentDto) {
+    return this.documentService
+      .createDocument(createDocumentDto)
+      .then((response) => {
+        return response;
+      })
+      .catch((error) => {
+        throw new HttpException('Произошла какая-то ошибка при создании документа =(: ' + error, HttpStatus.INTERNAL_SERVER_ERROR);
+      })
+  }
+}
--- src/documents/documents.service.ts
+++ src/documents/documents.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { DataSource, InsertResult } from 'typeorm';
+import { CreateDocumentDto } from './dto/create-document.dto';
+import { Document } from './entities/document.entity';
 
 @Injectable()
-export class DocumentsService {}
+export class DocumentsService {
+
+  constructor(private dataSource: DataSource) { }
+
+  async findAllDocuments(): Promise<Document[]> {
+    return await this.dataSource.getRepository(Document).find();
+  }
+  
+  async createDocument(createDocumentDto: CreateDocumentDto): Promise<InsertResult> {
+    return await this.dataSource
+      .createQueryBuilder()
+      .insert()
+      .into(Document)
+      .values([
+        {
+          cipher: createDocumentDto.cipher,
+          createdOn: createDocumentDto.createdOn,
+          inArchive: createDocumentDto.inArchive,
+        },
+      ])
+      .execute();
+  }  
+}
