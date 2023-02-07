87ab9c4d239aa43ccfdb5103cbb5d6458b5d7fc6
Зелёный Андрей Сергеевич <andrey.zelenyy@south.rt.ru>
2023-02-07 22:23:33 +0300

    ..

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(3000);
-  console.log('App is listening on port 3000...');
+  console.log('App is listening on port 3000..');
 
   if (module.hot) {
     module.hot.accept();
